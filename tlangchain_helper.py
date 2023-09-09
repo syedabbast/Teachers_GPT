987b5c06346001b2d453aee0f6f74fac8e78ba67
Syed Abbas <tahasyed110@gmail.com>
2023-09-09 14:47:09 -0400

    Add files via upload

---
+++ tlangchain_helper.py
@@ -0,0 +1,26 @@
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from keyfile import openai_key
+
+
+import os
+os.environ['OPENAI_API_KEY'] = openai_key
+
+llm = OpenAI(temperature=0.7)
+
+def generate_curriculum(level):
+    # Chain 1: Restaurant Name
+    prompt_template_curriculum = PromptTemplate(
+        input_variables=['level'],
+        template="Generate a curriculum outline for grade {level} in US standard elementary schools."
+    )
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_curriculum, output_key="level_name")
+    response = name_chain({'level': level})
+    return response
+
+
+if __name__ == "__main__":
+    
+    print(generate_curriculum("one"))  # Change "first" to the desired grade level
