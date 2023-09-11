2b0b0495ee0644edc1e0923c157db4d11e2f5ef4
Syed Abbas <tahasyed110@gmail.com>
2023-09-10 23:47:40 -0400

    Update tlangchain_helper.py

--- tlangchain_helper.py
+++ tlangchain_helper.py
@@ -5,6 +5,7 @@ from langchain.chains import SequentialChain
 import streamlit as st
 import os
 #from keyfile import openai_key
+os.environ['OPENAI_API_KEY'] = openai_key
 openai_key = os.environ.get("OPENAI_API_KEY")
 
 llm = OpenAI(temperature=0.7)
